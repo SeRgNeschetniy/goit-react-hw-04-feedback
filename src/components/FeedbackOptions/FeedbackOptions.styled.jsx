f9a848bc52a966f0d02ae13a7622469de4b64ffc
SeRgNeschetniy <52968351+SeRgNeschetniy@users.noreply.github.com>
2022-09-26 22:20:00 +0300

    Initial commit

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  padding: 5px 10px;
+  background-color: floralwhite;
+  font-size: 18px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+    background-color: grey;
+  }
+
+  &:not(:last-child) {
+    margin-right: 20px;
+  }
+`;
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const SectionWrapp = styled.section`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto 30px;
+  width: 400px;
+`;
